8b1b6ee139e8f336a4f5b327da281677db31205a
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-19 17:00:00 -0600

    Save answers 1-10. May need this version if refactoring gets away from me -MD

--- ruby_exercises/data-types/collections/advanced_nested_collections/spec/advanced_nesting_spec.rb
+++ ruby_exercises/data-types/collections/advanced_nested_collections/spec/advanced_nesting_spec.rb
@@ -16,62 +16,73 @@ RSpec.describe 'Advanced Nested Collections' do
   it 'test 1' do
     # EXAMPLE
     employees = stores[:olive_garden][:employees]
-
     expected = ["Jeff", "Zach", "Samantha"]
     expect(employees).to eq(expected)
   end
 
-  xit 'test 2' do
+  it 'test 2' do
     # Find the ingredients for pancakes
-    pancake_ingredients = _____
+    pancake_ingredients = stores[:dennys][:dishes][0][:ingredients]
 
     expected = ["Flour", "Eggs", "Milk", "Syrup"]
     expect(pancake_ingredients).to eq(expected)
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     # Find the price of risotto
-    risotto_price = ____
+    risotto_price = stores[:olive_garden][:dishes].select{ |dish| 
+      dish[:name].downcase == "risotto"
+    }[0][:price]
 
     expect(risotto_price).to eq(12)
   end
 
-  xit 'test 4' do
+  it 'test 4' do
     # Find the ingredients for a Big Mac
-    big_mac_ingredients = ____
+    big_mac_ingredients = stores[:macdonalds][:dishes].select{ |dish| 
+      dish[:name] == "Big Mac"
+  }[0][:ingredients]
 
     expected = ['Bun','Hamburger','Ketchup','pickles']
     expect(big_mac_ingredients).to eq(expected)
   end
 
-  xit 'test 5' do
+  it 'test 5' do
     # Find a list of restaurants
-    store_names = ____
+    store_names = stores.keys
 
     expected = [:olive_garden, :dennys, :macdonalds]
     expect(store_names).to eq(expected)
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     # Find dishes names for Olive Garden
-    dishes_names = ____
+    dishes_names = stores[:olive_garden][:dishes].map{ |dish|
+      dish[:name]
+  }
 
     expect(dishes_names).to eq(['Risotto', 'Steak'])
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     # Return a list of employees across
     # all restaurants
-    employee_names = ____
+    employee_names = stores.keys.map{ |store|
+        stores[store][:employees]
+    }.flatten
 
     expected = ["Jeff", "Zach", "Samantha", "Bob", "Sue", "James", "Alvin", "Simon", "Theodore"]
     expect(employee_names).to eq(expected)
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     # Return a list of all ingredients
     # across all restaurants
-    ingredients = ____
+    ingredients = stores.keys.map{ |store|
+        stores[store][:dishes].map{ |dish|
+        dish[:ingredients]
+    }
+  }.flatten
 
     expected = [
       "Rice",
@@ -96,18 +107,27 @@ RSpec.describe 'Advanced Nested Collections' do
     expect(ingredients).to eq(expected)
   end
 
-  xit 'test 9' do
+  it 'test 9' do
     # Return the full menu price for Olive Garden
-    full_menu_price = ____
+    full_menu_price = stores[:olive_garden][:dishes].map{ |dish|
+        dish[:price]
+    }.sum
 
     expect(full_menu_price).to eq(27)
   end
 
-  xit 'test 10' do
+  it 'test 10' do
     # Return the full menu for Olive Garden
+    def get_menu(store)
+      og_dishes = {}
+      store[:dishes].map{ |dish|
+          og_dishes[dish[:name]] = dish
+      }
+      return og_dishes
+    end
 
-    olive_garden_menu = _____
-    
+    olive_garden_menu = get_menu(stores[:olive_garden])
+
     expected = {
       "Risotto" => {
         :name => "Risotto",
@@ -123,9 +143,22 @@ RSpec.describe 'Advanced Nested Collections' do
     expect(olive_garden_menu).to eq(expected)
   end
 
-  xit 'test 11' do
+  it 'test 11' do
     # Return a full menu across all restaurants
-    full_menu = ____
+
+    def get_menus
+      menu_accum = {}
+      stores.each{ |store_name, store_obj| 
+        store_obj.each{ |k,v|
+          if k == :dishes
+              v.each{ |dish| menu_accum[dish[:name]] = v[0] }
+          end
+        }
+      }
+      return menu_accum
+    end
+
+    full_menu = get_menus
 
     expected = {
       "Risotto" => {
